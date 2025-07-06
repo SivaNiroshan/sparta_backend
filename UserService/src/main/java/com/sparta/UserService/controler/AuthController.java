074b9eb588a6246f13cbae6903db4d4cab4b223f
Niroshan <sniroshan1964@gmail.com>
2025-07-06 13:48:45 +0530

    Revert "User service Done"
    This reverts commit 920d59d836f53bee1d269ac0647d66b77063f770.

--- UserService/src/main/java/com/sparta/UserService/controler/AuthController.java
+++ UserService/src/main/java/com/sparta/UserService/controler/AuthController.java
@@ -1,18 +1,12 @@
 package com.sparta.UserService.controler;
 
 import com.sparta.UserService.model.LoginRequest;
-import com.sparta.UserService.model.SignupRequest;
 import com.sparta.UserService.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
 @RestController
-@RequestMapping("account/auth")
+@RequestMapping("api/auth")
 public class AuthController {
 
     @Autowired
@@ -23,31 +17,5 @@ public class AuthController {
     public Object login(@RequestBody LoginRequest request) {
         return authService.login(request.getEmail(), request.getPassword());
     }
-
-    @PostMapping("/signup")
-    public Object signup(@RequestBody SignupRequest request){
-        return authService.signup(request.getEmail(),request.getPassword(),request.getFirstname(),request.getLastname(),request.getUsername());
-
-    }
-
-    @GetMapping("/email-exists")
-    public boolean emailExists(@RequestParam String email) {
-        return authService.emailExists(email);
-    }
-
-    @PatchMapping("/updatepassword")
-    public ResponseEntity<String> changePassword(@RequestBody Map<String, String> request){
-        String userId = request.get("userId");
-        String newPassword = request.get("newPassword");
-
-        if (userId == null || newPassword == null) {
-            return ResponseEntity.badRequest().body("Missing userId or newPassword");
-        }
-
-        return authService.updatePasswordByUID(UUID.fromString(userId),newPassword);
-    }
-
-
-
 }
 
